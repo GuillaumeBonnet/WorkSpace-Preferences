2b7d915aba59e9bd0a3c593f6519bb028729fe3f
Guillaume Bonnet <7252481+GuillaumeBonnet@users.noreply.github.com>
2021-07-31 11:40:25 +0200

    Example-Startup-Script.bat

---
+++ scripts/Example-Startup-Script.bat
@@ -0,0 +1,5 @@
+cd C:\Users\gui\Documents\programmation\MetaMovie
+start /b /max code ./MetaMovie-Backend
+start /b /max code ./MetaMovie-Plugin
+start /b /max chrome --profile-directory="Profile 1"
+exit()
